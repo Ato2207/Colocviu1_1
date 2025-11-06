3773c7f1d834bc921f3c897e19d9991b41d85d5c
alexandru.atomei03 <atomeialexandru03@gmail.com>
2025-11-06 08:34:03 +0200

    Finished exercise A

---
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_1/Colocviu1_1MainActivity.kt
@@ -0,0 +1,15 @@
+package ro.pub.cs.systems.eim.Colocviu1_1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class Colocviu1_1MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_colocviu1_1_main)
+    }
+}
